e00768403efeddd21efbb2380dd98737cf30e26a
Hforna <henriqueflashpingo@gmail.com>
2024-07-01 23:16:25 -0300

    Testing and adding stripe api in project

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("/cart/delete/<int:pk>", delete_item_cart, name="delete_item_cart"),
     path("/shipping", shipping, name="shipping"),
     path("/new_shipping", edit_shipping, name="new_shipping"),
-    path("/payment", payment, name="payment")
+    path("/payment", payment, name="payment"),
+    path("/create-checkout-sessions", create_session, name="create_checkout"),
+    path("/config", stripe_config)
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,11 @@ from rolepermissions.decorators import has_role_decorator
 from django.contrib.auth.models import Group, User
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
+import stripe
+from django.http import HttpResponse
+from library_project import settings
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def cart(request):
@@ -62,6 +67,42 @@ def shipping(request):
 @login_required
 def payment(request):
     return render(request, "checkout/payment.html")
+
+@csrf_exempt
+def stripe_config(request):
+    if request.method == 'GET':
+        stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
+        return JsonResponse(stripe_config, safe=False)
+@csrf_exempt
+def create_session(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    if request.method == 'GET':
+        domain_url = 'http://localhost:8000/'
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        try:
+            # Create new Checkout Session for the order
+            # Other optional params include:
+            # [billing_address_collection] - to display billing address details on the page
+            # [customer] - if you have an existing Stripe Customer ID
+            # [payment_intent_data] - capture the payment later
+            # [customer_email] - prefill the email input in the form
+            # For full details see https://stripe.com/docs/api/checkout/sessions/create
+            # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+            checkout_session = stripe.checkout.Session.create(
+                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url + 'cancelled/',
+                payment_method_types=['card'],
+                mode='payment',
+                line_items=[
+                    {
+                        'quantity': 1,
+                        'price': "price_1PXlUF01ThTiNs0ZxH2XqjGE",
+                    }
+                ]
+            )
+            return JsonResponse({'sessionId': checkout_session['id']})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
     
 @login_required
 def edit_shipping(request):
