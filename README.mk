af1a053a9ca02a84b53e4421f10d3e014ca828ba
Hforna <henriqueflashpingo@gmail.com>
2024-07-05 16:11:09 -0300

    readme updated

--- README.mk
+++ README.mk
@@ -1,1 +1,9 @@
-Library Proeject in Django
+</h1> LIBRARY PROJECT IN DJANGO WITH STRIPE </h1>
+
+<p> That's a project created by me, i was created him in past and updated on few time
+I use Stripe api to create the payment process
+When the user go to the page him can choose if want be a writer or just a user
+If the user is a writer he can post his books in site and put a price
+If the user is just a user he can see his books in site and can buy them
+Later user buy a book or many books he go recive a email from the site and see if his payment was sucessfully received, together with the price of purchase </p>
+
